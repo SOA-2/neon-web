16e2e8a8ebf56b7043b09a4d4c264eeca50864f9
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-08 16:34:17 -0500

    Add home, log in, sign up and profile feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'signup',
+    loadChildren: () => import('@neon-web/sign-up').then((m) => m.signUpRoutes),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('@neon-web/log-in').then((m) => m.logInRoutes),
+  },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('@neon-web/profile').then((m) => m.profileRoutes),
+  },
+  {
+    path: '',
+    loadChildren: () => import('@neon-web/home').then((m) => m.homeRoutes),
+  },
+];
---
+++ src/app/log-in/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+
+export const logInRoutes: Route[] = [{ path: '', component: LogInComponent }];
