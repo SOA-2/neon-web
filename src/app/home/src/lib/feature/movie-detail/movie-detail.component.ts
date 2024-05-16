a22ce445f57d5ce3d829fbdc4c7df3b869baa991
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-16 15:30:07 -0500

    Improve movie detail

--- src/app/home/src/lib/feature/movie-detail/movie-detail.component.ts
+++ src/app/home/src/lib/feature/movie-detail/movie-detail.component.ts
@@ -4,12 +4,52 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '@neon-web/shared';
 
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'lib-movie-detail',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, MatExpansionModule],
+  imports: [CommonModule, HeaderComponent, MatExpansionModule, MatChipsModule],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.scss',
 })
-export class MovieDetailComponent {}
+export class MovieDetailComponent {
+  theaters = [
+    {
+      name: 'Molinos',
+      shows: [
+        { time: '12:00 PM' },
+        { time: '2:00 PM' },
+        { time: '2:40 PM' },
+        { time: '3:30 PM' }
+      ]
+    },
+    {
+      name: 'Oviedo',
+      shows: [
+        { time: '12:00 PM' },
+        { time: '2:00 PM' },
+        { time: '2:40 PM' },
+        { time: '3:30 PM' }
+      ]
+    },
+    {
+      name: 'Santafe',
+      shows: [
+        { time: '12:00 PM' },
+        { time: '2:00 PM' },
+        { time: '2:40 PM' },
+        { time: '3:30 PM' }
+      ]
+    },
+    {
+      name: 'Viva Envigado',
+      shows: [
+        { time: '12:00 PM' },
+        { time: '2:00 PM' },
+        { time: '2:40 PM' },
+        { time: '3:30 PM' }
+      ]
+    }
+  ]
+}
